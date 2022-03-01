1f38e268b6a803831acecb7d147e700ec80eeea9
Kiran <kiransettyks@gmail.com>
2022-03-01 21:19:46 +0200

    Add SQLAlchemy to main.py - Create Movie DB w/ Table structure Create an entry

--- main.py
+++ main.py
@@ -7,13 +7,44 @@ from wtforms.validators import DataRequired
 import requests
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
 
 
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), unique=True, nullable=False)
+    year = db.Column(db.Integer, unique=False, nullable=False)
+    description = db.Column(db.String(1500), unique=True, nullable=False)
+    rating = db.Column(db.Float, unique=False, nullable=False)
+    ranking = db.Column(db.Integer, unique=True, nullable=False)
+    review = db.Column(db.String(1500), unique=False, nullable=False)
+    img_url = db.Column(db.String(150), unique=True, nullable=False)
+
+db.create_all()
+
+# new_movie = Movie(
+#     title="Phone Booth",
+#     year=2002,
+#     description="Publicist Stuart Shepard finds "
+#                 "himself trapped in a phone booth, pinned down by an extortionist's "
+#                 "sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads "
+#                 "to a jaw-dropping climax.",
+#     rating=7.3,
+#     ranking=10,
+#     review="My favourite character was the caller.",
+#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+# )
+# db.session.add(new_movie)
+# db.session.commit()
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    movies = []
+    return render_template("index.html", movies=movies)
 
 
 if __name__ == '__main__':
